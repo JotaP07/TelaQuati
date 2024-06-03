16501df8af16064503a368db6a826b074d399ac0
JotaP07 <joao.pcanhete@gmail.com>
2024-06-03 15:39:31 -0300

    TelaQuati

---
+++ src/main/java/com/mycompany/telaquati/Quati.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.telaquati;
+
+/**
+ *
+ * @author joaop
+ */
+public class Quati {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
